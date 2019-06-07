d097fc6439cc8c3d282bd64ff0776923e1b092dd
kgeeknerdsley <kgeekmail@gmail.com>
2019-06-06 19:32:42 -0700

    Added stopwatch button

--- mwoc_programmer/mwoc_programmer/Form1.Designer.cs
+++ mwoc_programmer/mwoc_programmer/Form1.Designer.cs
@@ -28,13 +28,81 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.clickTimer = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.stopwatchText = new System.Windows.Forms.TextBox();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// clickTimer
+			// 
+			this.clickTimer.Location = new System.Drawing.Point(3, 56);
+			this.clickTimer.Name = "clickTimer";
+			this.clickTimer.Size = new System.Drawing.Size(155, 113);
+			this.clickTimer.TabIndex = 0;
+			this.clickTimer.Text = "Timer";
+			this.clickTimer.UseVisualStyleBackColor = true;
+			this.clickTimer.MouseDown += new System.Windows.Forms.MouseEventHandler(this.clickTimer_MouseDown);
+			this.clickTimer.MouseUp += new System.Windows.Forms.MouseEventHandler(this.clickTimer_MouseUp);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(164, 56);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(133, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Time button was held (ms):";
+			// 
+			// stopwatchText
+			// 
+			this.stopwatchText.Location = new System.Drawing.Point(180, 72);
+			this.stopwatchText.Name = "stopwatchText";
+			this.stopwatchText.Size = new System.Drawing.Size(100, 20);
+			this.stopwatchText.TabIndex = 2;
+			// 
+			// listView1
+			// 
+			this.listView1.Location = new System.Drawing.Point(830, 12);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(275, 605);
+			this.listView1.TabIndex = 3;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.clickTimer);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.stopwatchText);
+			this.panel1.Location = new System.Drawing.Point(12, 418);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(327, 199);
+			this.panel1.TabIndex = 4;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(1117, 629);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.listView1);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button clickTimer;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox stopwatchText;
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.Panel panel1;
 	}
 }
 
--- mwoc_programmer/mwoc_programmer/Form1.cs
+++ mwoc_programmer/mwoc_programmer/Form1.cs
@@ -6,15 +6,32 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace mwoc_programmer
 {
 	public partial class Form1 : Form
 	{
+
+		Stopwatch watch = new Stopwatch();
+
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+
+		private void clickTimer_MouseDown(object sender, MouseEventArgs e)
+		{
+			watch.Start();
+		}
+
+		private void clickTimer_MouseUp(object sender, MouseEventArgs e)
+		{
+			watch.Stop();
+			stopwatchText.Text = watch.ElapsedMilliseconds.ToString();
+			watch.Reset();
 		}
 	}
 }
