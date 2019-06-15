390b14b6e29ac3269aa44f124dc92f52f0f3b143
kgeeknerdsley <kgeekmail@gmail.com>
2019-06-14 18:08:55 -0700

    Added pattern generator, total time, various tweaks

--- mwoc_programmer/mwoc_programmer/Form1.Designer.cs
+++ mwoc_programmer/mwoc_programmer/Form1.Designer.cs
@@ -28,6 +28,8 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
 			this.label2 = new System.Windows.Forms.Label();
 			this.label3 = new System.Windows.Forms.Label();
 			this.label4 = new System.Windows.Forms.Label();
@@ -35,15 +37,24 @@
 			this.manual_ColorBox = new System.Windows.Forms.ComboBox();
 			this.manual_AddBtn = new System.Windows.Forms.Button();
 			this.cmdViewer = new System.Windows.Forms.DataGridView();
-			this.timeColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.powerCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.colorColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
 			this.stopwatchText = new System.Windows.Forms.TextBox();
 			this.label1 = new System.Windows.Forms.Label();
 			this.clickTimer = new System.Windows.Forms.Button();
 			this.label6 = new System.Windows.Forms.Label();
 			this.manual_TimeBox = new System.Windows.Forms.TextBox();
 			this.manual_PowerBox = new System.Windows.Forms.TextBox();
+			this.outputToFileBtn = new System.Windows.Forms.Button();
+			this.totalTimeColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.timeColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.powerCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.colorColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.patternBtnBox = new System.Windows.Forms.Button();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.patternColorBox = new System.Windows.Forms.ComboBox();
+			this.patternPowerBox = new System.Windows.Forms.TextBox();
+			this.patternListBtn = new System.Windows.Forms.Button();
 			((System.ComponentModel.ISupportInitialize)(this.cmdViewer)).BeginInit();
 			this.SuspendLayout();
 			// 
@@ -111,32 +122,26 @@
 			this.cmdViewer.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Sunken;
 			this.cmdViewer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
 			this.cmdViewer.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.totalTimeColumn,
             this.timeColumn,
             this.powerCol,
             this.colorColumn});
-			this.cmdViewer.Location = new System.Drawing.Point(809, 12);
+			dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+			dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+			dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+			dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+			dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+			dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+			this.cmdViewer.DefaultCellStyle = dataGridViewCellStyle4;
+			this.cmdViewer.Location = new System.Drawing.Point(848, 12);
 			this.cmdViewer.Name = "cmdViewer";
-			this.cmdViewer.Size = new System.Drawing.Size(351, 628);
+			this.cmdViewer.Size = new System.Drawing.Size(312, 594);
 			this.cmdViewer.TabIndex = 13;
-			// 
-			// timeColumn
-			// 
-			this.timeColumn.HeaderText = "Time";
-			this.timeColumn.Name = "timeColumn";
-			// 
-			// powerCol
-			// 
-			this.powerCol.HeaderText = "Power";
-			this.powerCol.Name = "powerCol";
-			// 
-			// colorColumn
-			// 
-			this.colorColumn.HeaderText = "Color";
-			this.colorColumn.Name = "colorColumn";
 			// 
 			// stopwatchText
 			// 
-			this.stopwatchText.Location = new System.Drawing.Point(217, 543);
+			this.stopwatchText.Location = new System.Drawing.Point(217, 580);
 			this.stopwatchText.Name = "stopwatchText";
 			this.stopwatchText.Size = new System.Drawing.Size(47, 20);
 			this.stopwatchText.TabIndex = 2;
@@ -144,7 +149,7 @@
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(173, 527);
+			this.label1.Location = new System.Drawing.Point(173, 564);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(133, 13);
 			this.label1.TabIndex = 1;
@@ -184,11 +189,119 @@
 			this.manual_PowerBox.Size = new System.Drawing.Size(34, 20);
 			this.manual_PowerBox.TabIndex = 16;
 			// 
+			// outputToFileBtn
+			// 
+			this.outputToFileBtn.Location = new System.Drawing.Point(848, 613);
+			this.outputToFileBtn.Name = "outputToFileBtn";
+			this.outputToFileBtn.Size = new System.Drawing.Size(312, 27);
+			this.outputToFileBtn.TabIndex = 17;
+			this.outputToFileBtn.Text = "Output show to file";
+			this.outputToFileBtn.UseVisualStyleBackColor = true;
+			// 
+			// totalTimeColumn
+			// 
+			dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+			this.totalTimeColumn.DefaultCellStyle = dataGridViewCellStyle3;
+			this.totalTimeColumn.HeaderText = "Total Time";
+			this.totalTimeColumn.Name = "totalTimeColumn";
+			this.totalTimeColumn.ToolTipText = "Total show time in seconds";
+			// 
+			// timeColumn
+			// 
+			this.timeColumn.HeaderText = "Time";
+			this.timeColumn.Name = "timeColumn";
+			// 
+			// powerCol
+			// 
+			this.powerCol.HeaderText = "Power";
+			this.powerCol.Name = "powerCol";
+			// 
+			// colorColumn
+			// 
+			this.colorColumn.HeaderText = "Color";
+			this.colorColumn.Name = "colorColumn";
+			// 
+			// patternBtnBox
+			// 
+			this.patternBtnBox.Location = new System.Drawing.Point(12, 173);
+			this.patternBtnBox.Name = "patternBtnBox";
+			this.patternBtnBox.Size = new System.Drawing.Size(128, 100);
+			this.patternBtnBox.TabIndex = 18;
+			this.patternBtnBox.Text = "Tap out pattern";
+			this.patternBtnBox.UseVisualStyleBackColor = true;
+			this.patternBtnBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.patternBtn_MouseDown);
+			this.patternBtnBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.patternBtn_MouseUp);
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(15, 144);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(91, 13);
+			this.label7.TabIndex = 19;
+			this.label7.Text = "Pattern Generator";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(146, 173);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(40, 13);
+			this.label8.TabIndex = 20;
+			this.label8.Text = "Power:";
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(149, 202);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(34, 13);
+			this.label9.TabIndex = 21;
+			this.label9.Text = "Color:";
+			// 
+			// patternColorBox
+			// 
+			this.patternColorBox.FormattingEnabled = true;
+			this.patternColorBox.Items.AddRange(new object[] {
+            "Red",
+            "Blue",
+            "Green",
+            "Yellow"});
+			this.patternColorBox.Location = new System.Drawing.Point(189, 202);
+			this.patternColorBox.Name = "patternColorBox";
+			this.patternColorBox.Size = new System.Drawing.Size(58, 21);
+			this.patternColorBox.TabIndex = 22;
+			// 
+			// patternPowerBox
+			// 
+			this.patternPowerBox.Location = new System.Drawing.Point(189, 173);
+			this.patternPowerBox.Name = "patternPowerBox";
+			this.patternPowerBox.Size = new System.Drawing.Size(58, 20);
+			this.patternPowerBox.TabIndex = 23;
+			// 
+			// patternListBtn
+			// 
+			this.patternListBtn.Location = new System.Drawing.Point(152, 238);
+			this.patternListBtn.Name = "patternListBtn";
+			this.patternListBtn.Size = new System.Drawing.Size(95, 35);
+			this.patternListBtn.TabIndex = 24;
+			this.patternListBtn.Text = "Add to list";
+			this.patternListBtn.UseVisualStyleBackColor = true;
+			this.patternListBtn.Click += new System.EventHandler(this.patternListBtn_Click);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(1172, 652);
+			this.Controls.Add(this.patternListBtn);
+			this.Controls.Add(this.patternPowerBox);
+			this.Controls.Add(this.patternColorBox);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.patternBtnBox);
+			this.Controls.Add(this.outputToFileBtn);
 			this.Controls.Add(this.manual_PowerBox);
 			this.Controls.Add(this.manual_TimeBox);
 			this.Controls.Add(this.label6);
@@ -203,7 +316,7 @@
 			this.Controls.Add(this.label3);
 			this.Controls.Add(this.label2);
 			this.Name = "Form1";
-			this.Text = "Form1";
+			this.Text = "Mini WoC Programmer";
 			this.Load += new System.EventHandler(this.Form1_Load);
 			((System.ComponentModel.ISupportInitialize)(this.cmdViewer)).EndInit();
 			this.ResumeLayout(false);
@@ -219,15 +332,24 @@
 		private System.Windows.Forms.ComboBox manual_ColorBox;
 		private System.Windows.Forms.Button manual_AddBtn;
 		private System.Windows.Forms.DataGridView cmdViewer;
-		private System.Windows.Forms.DataGridViewTextBoxColumn timeColumn;
-		private System.Windows.Forms.DataGridViewTextBoxColumn powerCol;
-		private System.Windows.Forms.DataGridViewTextBoxColumn colorColumn;
 		private System.Windows.Forms.TextBox stopwatchText;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Button clickTimer;
 		private System.Windows.Forms.Label label6;
 		private System.Windows.Forms.TextBox manual_TimeBox;
 		private System.Windows.Forms.TextBox manual_PowerBox;
+		private System.Windows.Forms.Button outputToFileBtn;
+		private System.Windows.Forms.DataGridViewTextBoxColumn totalTimeColumn;
+		private System.Windows.Forms.DataGridViewTextBoxColumn timeColumn;
+		private System.Windows.Forms.DataGridViewTextBoxColumn powerCol;
+		private System.Windows.Forms.DataGridViewTextBoxColumn colorColumn;
+		private System.Windows.Forms.Button patternBtnBox;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.ComboBox patternColorBox;
+		private System.Windows.Forms.TextBox patternPowerBox;
+		private System.Windows.Forms.Button patternListBtn;
 	}
 }
 
--- mwoc_programmer/mwoc_programmer/Form1.cs
+++ mwoc_programmer/mwoc_programmer/Form1.cs
@@ -26,9 +26,32 @@ namespace mwoc_programmer
 		List<int> power = new List<int>();
 		List<int> color = new List<int>();
 
+		List<long> patternTime = new List<long>();
+		List<int> patternPower = new List<int>();
+
+		double totalShowTime = 0;
+
+		
+
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			List<int> testList = new List<int>() { 1, 2, 3 };
+
+			//sets the column width to a more reasonable size
+			for (int i = 0; i < 4; i++)
+			{
+				cmdViewer.Columns[i].Width = 60;
+			}
+
+			//cmdViewer.Rows[0].Cells[0].Value = testList[0];
+
+
+			//stopwatchText.Text = cmdViewer.Rows[0].Cells[2].Value.ToString();
 		}
 
 
@@ -48,16 +71,84 @@ namespace mwoc_programmer
 
 		private void manualAddToList_Click(object sender, EventArgs e)
 		{
-			cmdViewer.Rows.Add(manual_TimeBox.Text, manual_PowerBox.Text, manual_ColorBox.Text);
+			updateTotalTime(double.Parse(manual_TimeBox.Text));
+			cmdViewer.Rows.Add(totalShowTime,manual_TimeBox.Text, manual_PowerBox.Text, manual_ColorBox.Text);
 		}
 
-		private void Form1_Load(object sender, EventArgs e)
+		
+
+		//updates total time, only provide it time in ms!
+		public void updateTotalTime(double addedTime)
 		{
-			List<int> testList = new List<int>() { 1, 2, 3 };
-			//cmdViewer.Rows[0].Cells[0].Value = testList[0];
+			totalShowTime += (addedTime * 0.001);
+		}
+
+		private void patternBtn_MouseUp(object sender, MouseEventArgs e)
+		{
+			if(watch.IsRunning)
+			{
+				//system.formatexception
+				watch.Stop();
+				
+
+				try
+				{
+					patternTime.Add(watch.ElapsedMilliseconds);
+					patternPower.Add(int.Parse(patternPowerBox.Text));
+				}
+				catch(FormatException)
+				{
+					MessageBox.Show("Must enter a valid power and color value");
+					watch.Stop();
+					patternTime.Clear();
+					patternPower.Clear();
+					watch.Reset();
+				}
+				
+				watch.Reset();
+			}
+
+			watch.Start();
+		}
+
+		private void patternBtn_MouseDown(object sender, MouseEventArgs e)
+		{
+			//if watch is running, this means it's counting the off time
+			if(watch.IsRunning)
+			{
+				watch.Stop();
+				patternTime.Add(watch.ElapsedMilliseconds);
+				patternPower.Add(0);
+				watch.Reset();
+
+			}
+
+			watch.Start();
+
+		}
+
+		private void patternListBtn_Click(object sender, EventArgs e)
+		{
+			if(watch.IsRunning)
+			{
+				watch.Stop();
+			}
+
+			watch.Reset();
 
 			
-			//stopwatchText.Text = cmdViewer.Rows[0].Cells[2].Value.ToString();
+
+			for(int i = 0; i < patternTime.Count; i++)
+			{
+				updateTotalTime(patternTime[i]);
+				cmdViewer.Rows.Add(totalShowTime, patternTime[i], patternPower[i], patternColorBox.Text);
+			}
+
+			//updateTotalTime(10);
+			//cmdViewer.Rows.Add(totalShowTime, 10, 0, patternColorBox.Text);
+
+			patternTime.Clear();
+			patternPower.Clear();
 		}
 	}
 }
